98c5977f19cb04899164927680e0692cccd0eaaf
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-06 10:09:06 -0800

    slight clean up

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.set('views', __dirname + '/views')
 
 app.set('view engine', 'jsx')
 
-//
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
